a30f1d59cec538b667db4f5e2a58feff37360f5d
Stephen Davies <stephen.davies@codemate.com>
2026-05-05 14:48:25 +0300

    Changing to a better contrast colors

--- nvim/.config/nvim/lua/color.lua
+++ nvim/.config/nvim/lua/color.lua
@@ -1,4 +1,4 @@
-vim.cmd.colorscheme("everforest")
+vim.cmd.colorscheme("onedark")
 
 vim.api.nvim_set_hl(0, "NonText", { ctermfg = 81, fg = "#6dceeb" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { link = "NonText" })
